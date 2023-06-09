6c54566e31c761de53be12e72ce09edbae7f1e50
viua <sysfs@yahoo.com>
2023-06-10 00:45:33 +0200

    impl simple accumulator

--- src/main/java/com/metapack/assignments/accumulator/impl/SimpleAccumulator.java
+++ src/main/java/com/metapack/assignments/accumulator/impl/SimpleAccumulator.java
@@ -2,22 +2,35 @@ package com.metapack.assignments.accumulator.impl;
 
 import com.metapack.assignments.accumulator.api.Accumulator;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class SimpleAccumulator implements Accumulator {
 
+    private int total;
+
     @Override
     public int accumulate(int... values) {
-        return 0;
+        return Optional.ofNullable(values)
+                .map(Arrays::stream)
+                .map(IntStream::sum)
+                .map(sum -> {
+                    this.total = Math.addExact(this.total, sum);
+                    return sum;
+                })
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     @Override
     public int getTotal() {
-        return 0;
+        return this.total;
     }
 
     @Override
     public void reset() {
-
+        this.total = 0;
     }
+
 
 }
